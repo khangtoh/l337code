13d547528d831d9c7ccca8edd5dc92744dda7558
Khang Toh <khang@formativeai.com>
2020-01-09 09:35:21 +0800

    Add minsumpath

---
+++ minsumpath/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var grid = [][]int{{1, 2, 2}, {13, 1, 1}}
+	end := findEndPoint(grid)
+	fmt.Println(end.X, end.Y)
+}
+
+// Coord stores the X, Y values for a coord
+// of a M by N matric array
+type Coord struct {
+	X int
+	Y int
+}
+
+func minPathSum(grid [][]int) int {
+	endCoord := findEndPoint(grid)
+	if endCoord.X == 0 && endCoord.Y == 0 {
+		return 0
+	}
+	return 0
+
+}
+
+func findEndPoint(grid [][]int) Coord {
+	depth := len(grid)
+	breadth := len(grid[0])
+	if breadth != 0 && depth != 0 {
+		return Coord{breadth, depth}
+	}
+	return Coord{0, 0}
+}
