79b37448f967daa3624b41c5b98d27787e04ba18
Khang Toh <khang.toh@gmail.com>
2020-01-11 01:39:48 +0800

    Final soln for minsumpath with tests & test cases

--- minsumpath/main.go
+++ minsumpath/main.go
@@ -16,19 +16,64 @@ type Coord struct {
 }
 
 // MinPathSum func
-func MinPathSum(grid [][]int) int {
+func MinPathSum(grid [][]int, currentCoord Coord, cost int) int {
 	endCoord := FindEndPoint(grid)
-	if endCoord.X == 0 && endCoord.Y == 0 {
+	var coord1 Coord
+	var coord2 Coord
+	var cost1 int
+	var cost2 int
+
+	if currentCoord.X > endCoord.X || currentCoord.Y > endCoord.Y {
 		return 0
 	}
-	return 0
+
+	if endCoord.X == 0 && endCoord.Y == 0 {
+		return grid[0][0]
+	}
+
+	coord1 = Coord{currentCoord.X + 1, currentCoord.Y}
+	coord2 = Coord{currentCoord.X, currentCoord.Y + 1}
+
+	// if already at endpoint
+	if currentCoord == endCoord {
+		return cost
+	}
+
+	// if no more left moves
+	if currentCoord.X == endCoord.X {
+		return MinPathSum(grid, coord2, cost+grid[coord2.Y][coord2.X])
+	}
+	// if no more down moves
+	if currentCoord.Y == endCoord.Y {
+		return MinPathSum(grid, coord1, cost+grid[coord1.Y][coord1.X])
+	}
+
+	// try both moves and return the lower cost
+	cost1 = MinPathSum(grid, coord1, cost+grid[coord1.Y][coord1.X])
+	cost2 = MinPathSum(grid, coord2, cost+grid[coord2.Y][coord2.X])
+
+	if cost2 < cost1 {
+		return cost2
+	}
+	return cost1
+
+	// if currentCoord.X+1 == endCoord.X {
+	// 	coord2 = Coord{currentCoord.X, currentCoord.Y + 1}
+	// 	return MinPathSum(grid, coord2, cost+grid[coord2.Y][coord2.X])
+	// }
+	// if currentCoord.Y+1 == endCoord.Y {
+	// 	coord1 = Coord{currentCoord.X + 1, currentCoord.Y}
+	// 	return MinPathSum(grid, coord1, cost+grid[coord1.Y][coord1.X])
+	// }
+
+	//return 0
 
 }
 
 // FindEndPoint func
 func FindEndPoint(grid [][]int) Coord {
-	depth := len(grid)
-	breadth := len(grid[0])
+	depth := len(grid) - 1
+	breadth := len(grid[0]) - 1
 	if breadth != 0 && depth != 0 {
 		return Coord{breadth, depth}
 	}
--- minsumpath/main_test.go
+++ minsumpath/main_test.go
@@ -6,23 +6,43 @@ var testCases = []struct {
 	grid [][]int
 }{
 	{
-		[][]int{
-			{1, 2}, {1, 1},
-			{1, 2, 1}, {1, 1, 1},
-		},
+		[][]int{{1, 2}, {1, 1}},
+	},
+	{
+		[][]int{{1, 2, 1}, {1, 1, 1}},
+	},
+	{
+		[][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}},
+	},
+	{
+		[][]int{{1, 1, 1}, {1, 3, 2}, {1, 1, 5}},
 	},
 }
 
 func Test(t *testing.T) {
-	tc := testCases[0]
-	cell := FindEndPoint(tc.grid)
-	if cell.X != 2 && cell.Y != 2 {
-		t.Fatalf("Winner is not X and should be X")
+	tc1 := testCases[0]
+	cell := FindEndPoint(tc1.grid)
+	if cell.X != 1 && cell.Y != 1 {
+		t.Fatalf("endPoint cell is not {1,1}")
 	}
-	tc = testCases[1]
-	cell = FindEndPoint(tc.grid)
-	if cell.X != 3 && cell.Y != 3 {
-		t.Fatalf("Winner is not X and should be X")
+	tc2 := testCases[1]
+	cell = FindEndPoint(tc2.grid)
+	if cell.X != 2 && cell.Y != 1 {
+		t.Fatalf("endPoint cell is not {2,1}")
 	}
 
+	cost := MinPathSum(tc1.grid, Coord{0, 0}, tc1.grid[0][0])
+	if cost != 3 {
+		t.Fatalf("tc2 minPath sum %d is not 3", cost)
+	}
+
+	tc3 := testCases[2]
+	if cost = MinPathSum(tc3.grid, Coord{0, 0}, tc3.grid[0][0]); cost != 7 {
+		t.Fatalf("tc2 minPath sum %d is not 7", cost)
+	}
+
+	tc4 := testCases[3]
+	if cost = MinPathSum(tc4.grid, Coord{0, 0}, tc4.grid[0][0]); cost != 9 {
+		t.Fatalf("tc2 minPath sum %d is not 9", cost)
+	}
 }
