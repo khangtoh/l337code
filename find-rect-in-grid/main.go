104759d16b81fac68999e00e38aa2a4e8c4c7733
Khang Toh <khang.toh@gmail.com>
2020-01-14 16:35:55 +0800

    Working soln for hasRectangle for a grid

--- find-rect-in-grid/main.go
+++ find-rect-in-grid/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -10,39 +9,85 @@ func main() {
 	mat := [][]int{
 		{1, 0, 0, 1, 0},
 		{0, 0, 1, 0, 1},
-		{0, 0, 0, 1, 0},
+		{1, 0, 0, 1, 0},
 		{1, 0, 1, 0, 1},
 	}
+	//fmt.Println(hasSameBits(mat[0], mat[1]))
+	//fmt.Println(hasSameBits(mat[0], mat[2]))
 
-	fmt.Println(isRectangle(mat) == true)
+	//fmt.Println(isRectangle(mat))
+
+	mat = [][]int{
+		{1, 0, 1},
+		{0, 1, 0},
+		{1, 0, 1},
+	}
+
+	//fmt.Println(isRectangle(mat))
+
+	mat = [][]int{
+		{1, 0, 0},
+		{1, 0, 0},
+		{1, 1, 0},
+	}
+
+	fmt.Println(isRectangle(mat))
 }
 
 func isRectangle(grid [][]int) bool {
-	rows := len(grid)
-	cols := len(grid[0])
-	var rowInts []int
-	// scans through each row
-	for i := 0; i < rows; i++ {
-		row := grid[i]
-		rowBits := 0
-		for j := 0; j < cols; j++ {
-			rowBits = rowBits | row[j]<<j
-			// if row[j] == 1 {
-			// 	rowBits = rowBits | row[j]<<j
-			// } else {
-			// 	rowBits = rowBits << 1
-			// }
 
+	rows := len(grid)
+	// for each row, check if there is a similar row
+	// with same bit patterm
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < rows; j++ {
+			if j != i {
+				if hasAtLeastTwoSameBits(grid[i], grid[j]) {
+					return true
+				}
+			}
 		}
-		rowInts = append(rowInts, reverseBits(rowBits))
-		fmt.Println("Row ", i, strconv.FormatInt(int64(rowBits), 2))
 	}
-	var hasRect int = 0
-	for _, rowInt := range rowInts {
-		hasRect = rowInt & hasRect
-	}
-	return hasRect != 0
+	return false
 }
+
+// func isRectangle(grid [][]int) bool {
+// 	rows := len(grid)
+// 	cols := len(grid[0])
+// 	var rowInts []int
+// 	// scans through each row
+// 	for i := 0; i < rows; i++ {
+// 		row := grid[i]
+// 		rowBits := 0
+// 		for j := 0; j < cols; j++ {
+// 			rowBits = rowBits<<1 | row[j]
+// 		}
+// 		rowInts = append(rowInts, rowBits)
+// 	}
+// 	var hasRect int = 0
+// 	for _, rowInt := range rowInts {
+// 		hasRect = rowInt | hasRect
+// 	}
+// 	return hasRect != 0
+// }
+
+func hasAtLeastTwoSameBits(a []int, b []int) bool {
+	cols := len(a)
+	aInt := 0
+	bInt := 0
+	sameBits := 0
+	for j := 0; j < cols; j++ {
+		if (a[j] & b[j]) == 1 {
+			sameBits++
+		}
+		aInt = aInt<<1 | a[j]
+		bInt = bInt<<1 | b[j]
+	}
+	return (aInt&bInt) != 0 && sameBits > 1
+	//return sameBits > 1
+}
+
 func reverseBits(b byte) byte {
 	var d byte
 	for i := 0; i < 8; i++ {
